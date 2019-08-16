7eb4fa7ad85be51192ccaa194804da38b2498066
Hooks Zhang <hookszhang@gmail.com>
2019-08-16 13:10:27 +0800

    feat: support string_array/number_array type args

--- src/args.js
+++ src/args.js
@@ -8,35 +8,48 @@ export default class Args {
     const argSpec = this._hasArg(name)
     if (!argSpec) throw new Error('Undefined arg name [' + name + '] in the schema')
 
+    const argIndex = this._findArgIndex(name)
     switch (argSpec.type) {
       case 'boolean':
-        return this._boolanValue(argSpec.name)
+        return this._boolanValue(argIndex)
       case 'number':
-        return this._numberValue(argSpec.name)
+        return this._numberValue(argIndex)
       case 'string':
-        return this._stringValue(argSpec.name)
+        return this._stringValue(argIndex)
+      case 'string_array':
+        return this._stringArrayValue(argIndex)
+      case 'number_array':
+          return this._numberArrayValue(argIndex)
       default:
         throw new Error('Unexpected arg type: ' + argSpec.type)
     }
   }
 
-  _boolanValue (name) {
-    const index = this._findArgIndex(name)
-    return index !== -1
+  _stringArrayValue (argIndex) {
+    // default value
+    if (argIndex === -1) return []
+    const valueText = this.argsArray[argIndex + 1]
+    return valueText.split(',')
   }
 
-  _numberValue (name) {
-    const index = this._findArgIndex(name)
-    // default value
-    if (index === -1) return 0
-    return Number(this.argsArray[index + 1])
+  _numberArrayValue (argIndex) {
+    return this._stringArrayValue(argIndex).map(arg => Number(arg))
   }
 
-  _stringValue (name) {
-    const index = this._findArgIndex(name)
+  _boolanValue (argIndex) {
+    return argIndex !== -1
+  }
+
+  _numberValue (argIndex) {
     // default value
-    if (index === -1) return ''
-    return this.argsArray[index + 1]
+    if (argIndex === -1) return 0
+    return Number(this.argsArray[argIndex + 1])
+  }
+
+  _stringValue (argIndex) {
+    // default value
+    if (argIndex === -1) return ''
+    return this.argsArray[argIndex + 1]
   }
 
   _findArgIndex (name) {
--- test/args.test.js
+++ test/args.test.js
@@ -2,8 +2,8 @@ import Args from '../src/args'
 import assert from 'assert'
 import Schema from '../src/schema';
 
-const schema = new Schema('l:boolean p:number d:string')
-const argsArray = ['-l', '-p', '8080', '-d', '/usr/logs']
+const schema = new Schema('l:boolean p:number g:string_array d:number_array')
+const argsArray = ['-l', '-p', '8080', '-g', 'this,is,a,list', '-d', '1,2,-3,5']
 describe('Args', function () {
   it('should return Args Object', function () {
     const args = new Args(schema, argsArray)
@@ -17,8 +17,16 @@ describe('Args', function () {
     const args = new Args(schema, argsArray)
     assert.equal(args.value('p'), 8080)
   })
-  it ('should return \'/usr/logs\' when get arg d', function () {
+  it ('should return string array when get arg g', function () {
     const args = new Args(schema, argsArray)
-    assert.equal(args.value('d'), '/usr/logs')
+    const value = args.value('g')
+    assert.ok(value instanceof Array)
+    assert.equal(JSON.stringify(value), JSON.stringify(['this', 'is', 'a', 'list']))
+  })
+  it ('should return number array when get arg d', function () {
+    const args = new Args(schema, argsArray)
+    const value = args.value('d')
+    assert.ok(value instanceof Array)
+    assert.equal(JSON.stringify(value), JSON.stringify([1, 2, -3, 5]))
   })
 })
